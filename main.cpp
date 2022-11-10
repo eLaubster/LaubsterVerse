7679a565b57dc45a1eeef1c9a7928e5e83eeeed6
Evan Laube <laubeevan@gmail.com>
2022-11-10 01:34:33 -0600

    Setup Raylib window

--- main.cpp
+++ main.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2022 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+
+#define WIDTH 1280
+#define HEIGHT 720
 
 int main(void)
 {
-    InitWindow(600, 300, "LaubsterVerse");
+    InitWindow(WIDTH, HEIGHT, "LaubsterVerse");
+    SetTargetFPS(60);
+//    int display = GetCurrentMonitor();
+//    SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+//    ToggleFullscreen();
+
+    float r = 20;
+    float bx = r;
+    float by = r;
+    float velx = 5;
+    float vely = 5;
 
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("It finally works", 100, 120, 35, RED);
+        DrawCircle(bx, by, 20, BLUE);
+        DrawRectangle(GetMouseX()-5, GetMouseY()-5, 10, 10, GREEN);
+        bx += velx;
+        by += vely;
+
+        if(bx > WIDTH-r || bx < r) velx *= -1;
+        if(by > HEIGHT-r || by < r) vely *= -1;
+
         EndDrawing();
     }
 
